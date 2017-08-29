b6cc9ad68469503ac4ba9fa5f07089db8e3b2087
Shubham Nishad <shubhamnishad25@gmail.com>
2017-08-29 17:54:07 +0530

    added some time fields to the model
    added updated and timestamp to URL model.

--- urlShortener/shortener/models.py
+++ urlShortener/shortener/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class shortenedUrl(models.Model):
     url = models.CharField(max_length=300)
     short =models.CharField(max_length=15,unique=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return str(self.url)
