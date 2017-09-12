8b4c57ac9b91204eb989fd4d60c2c2b1c1991ed0
Shubham Nishad <shubhamnishad25@gmail.com>
2017-09-12 19:33:01 +0530

    added bootstrap magic

--- src/urlShortener/shortener/forms.py
+++ src/urlShortener/shortener/forms.py
@@ -2,4 +2,7 @@ from django import forms
 from .validators import valid_URL
 
 class SubmitUrl(forms.Form):
-    url = forms.CharField(label="Submit URL",validators=[valid_URL])
+    url = forms.CharField(label="Submit URL",
+                          validators=[valid_URL],
+                          widget= forms.TextInput(attrs={"placeholder":"Long URL",
+                                                         "class":"form-control"}))
